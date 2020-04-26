13ecad737daf68e8dd2e0d6a8a097dcb839fcad2
Fran1611 <francisco.ibanez@correo.ucu.edu.uy>
2020-04-26 08:23:33 -0300

    comentarios en interfaces sobre patrones y principios

--- src/Library/Characters/ICharacter.cs
+++ src/Library/Characters/ICharacter.cs
@@ -2,10 +2,14 @@ using System;
 
 namespace RoleplayGame
 {
+    /*Se crea la interfaz ICharacter con todas las oepraciones que tienen en comun los personajes.
+    Los personajes que implementan esta interfaz ahora serán también de tipo ICharacter.
+    Las operaciones son polimorifcas porque son implementadas por dos o mas objetos de diferentes tipos.
+    Cumple además con LSP ya que el código funciona de igual manera cuando el objeto es subtipo de ICharacter.*/
     public interface ICharacter
     {
-        string Name {get; set; }
-        int Health {get; }
+        string Name {get;}
+        int Health {get;}
         int AttackValue {get;} 
         int DefenseValue {get;} 
         void ReceiveAttack(int a);
