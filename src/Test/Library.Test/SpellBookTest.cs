bf9b983468000591104d0fbe191964cb0d149801
Fran1611 <francisco.ibanez@correo.ucu.edu.uy>
2020-04-26 20:45:56 -0300

    Subo algunos test mas para spellsbook

--- src/Test/Library.Test/SpellBookTest.cs
+++ src/Test/Library.Test/SpellBookTest.cs
@@ -11,6 +11,7 @@ namespace Library.Test
             
         }
 
+        // Test que verifica el valor de ataque del libro de hechizos con 1 hechizo.
          [Test]
          public void SpellBookAttackValue_ExpectedValue()
         {   
@@ -21,6 +22,7 @@ namespace Library.Test
             Assert.AreEqual(expected,spellBookAttack);
         }
 
+        // Test que verifica el valor de defensa del libro de hechizos con 1 hechizo.
         [Test]
          public void SpellBookDefenseValue_ExpectedValue()
         {   
@@ -31,5 +33,48 @@ namespace Library.Test
             Assert.AreEqual(expected,spellBookDefense);
         }
 
+        // Test que verifica el valor de ataque del libro de hechizos con 2 hechizos.
+        [Test]
+        public void SpellBookAttackValue_With_Two_Spells()
+        {
+            SpellsBook spellsBook = new SpellsBook();
+            spellsBook.Spells = new Spell[]{new Spell(), new Spell()};
+            int spellBookAttack = spellsBook.AttackValue;
+            int expected = 140;
+            Assert.AreEqual(expected, spellBookAttack); 
+        }
+
+        // Test que verifica el valor de defensa del libro de hechizos con 2 hechizo.
+        [Test]
+        public void SpellBookDefenseValue_Whit_Two_Spells()
+        {   
+            SpellsBook spellBook = new SpellsBook();
+            spellBook.Spells = new Spell[]{ new Spell(), new Spell() };
+            int spellBookDefense = spellBook.DefenseValue;
+            int expected = 140;
+            Assert.AreEqual(expected,spellBookDefense);
+        }
+
+        // Test que verifica el valor de defensa del libro de hechizos con 0 hechizos.
+        [Test]
+        public void SpellBookDefenseValue_No_Spells()
+        {   
+            SpellsBook spellBook = new SpellsBook();
+            spellBook.Spells = new Spell[0];
+            int spellBookDefense = spellBook.DefenseValue;
+            int expected = 0;
+            Assert.AreEqual(expected,spellBookDefense);
+        }
+
+        // Test que verifica el valor de ataque del libro de hechizos con 0 hechizos.
+        [Test]
+        public void SpellBookAttackValue_No_Spells()
+        {   
+            SpellsBook spellBook = new SpellsBook();
+            spellBook.Spells = new Spell[0];
+            int spellBookAttack = spellBook.AttackValue;
+            int expected = 0;
+            Assert.AreEqual(expected,spellBookAttack);
+        }
     }
 }
