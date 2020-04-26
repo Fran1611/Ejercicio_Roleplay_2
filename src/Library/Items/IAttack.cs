ab7a9b4cf5723398a578eb786288e782b1f46f7c
Fran1611 <francisco.ibanez@correo.ucu.edu.uy>
2020-04-26 20:06:14 -0300

    arreglo comentarios que puse mal

--- src/Library/Items/IAttack.cs
+++ src/Library/Items/IAttack.cs
@@ -3,7 +3,7 @@ namespace RoleplayGame
     /*Se crea la interfaz IAttack con la oepración que tienen en comun los Items con valor de ataque.
     Los Items que implementan esta interfaz ahora serán también de tipo IAttack.
     La operación es polimorifica porque es implementada por dos o mas objetos de diferentes tipos.
-    Cumple además con LSP ya que el código funciona de igual manera cuando el objeto es subtipo de IDefense.*/
+    Cumple además con LSP ya que el código funciona de igual manera cuando el objeto es subtipo de IAttack.*/
     public interface IAttack
     {
         int AttackValue{get;}
--- src/Library/Items/IDefense.cs
+++ src/Library/Items/IDefense.cs
@@ -1,6 +1,6 @@
 namespace RoleplayGame
 {
-    /*Se crea la interfaz ICharacter con operacion que tienen en comun todos los Items con valor de defensa.
+    /*Se crea la interfaz IDefense con operacion que tienen en comun todos los Items con valor de defensa.
     Los Items que implementan esta interfaz ahora serán también de tipo IDefense.
     La operación es polimorifca porque es implementada por dos o mas objetos de diferentes tipos.
     Cumple además con LSP ya que el código funciona de igual manera cuando el objeto es subtipo de IDefense.*/
